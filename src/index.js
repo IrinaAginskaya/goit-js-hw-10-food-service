f2361db36cf9a6a11143d918614d839c18bb805a
IrinaAginskaya <aginska@gmail.com>
2021-10-15 10:48:13 +0300

    version for check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import './styles.css';
-// import './js/theme';
+import './js/theme';
 import './js/menu';
--- src/js/theme.js
+++ src/js/theme.js
@@ -3,3 +3,44 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
+const switchItem = document.querySelector('#theme-switch-toggle');
+
+switchItem.addEventListener('change', changeTheme);
+
+function changeTheme(evt)
+{  const checkEvt = evt.currentTarget.checked;
+  if (checkEvt) {
+    checkedTheme();
+  }
+  else {
+    notCheckedTheme();
+  }
+}
+
+  function checkedTheme() {
+  document.body.classList.add(Theme.DARK);
+  document.body.classList.remove(Theme.LIGHT);
+  localStorage.setItem('theme', Theme.DARK);
+  switchItem.checked = true;
+}
+
+function notCheckedTheme() {
+  document.body.classList.add(Theme.LIGHT);
+  document.body.classList.remove(Theme.DARK);
+  localStorage.setItem('theme', Theme.LIGHT);
+  switchItem.checked = false;
+}
+
+function currentTheme() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === Theme.LIGHT) {
+    notCheckedTheme();
+    return;
+  }
+
+  if (savedTheme === Theme.DARK) {
+    checkedTheme();
+    return;
+  }
+}
+currentTheme();
